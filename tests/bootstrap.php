eca0bbdfa1e149385932d61427d1237a0bd15f6e
Florent Morselli <contact@spomky-labs.com>
2025-11-07 12:08:33 +0100

    chore: update CI configurations, add new YAML files, and enhance PHP … (#50)
    * refactor: update DecodingTest to use mixed type for expectedNormalizedValue and improve input values
    * refactor: update DecodingTest to use mixed type for expectedNormalizedValue and improve input values

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,11 @@ declare(strict_types=1);
 
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
-ErrorHandler::register(null, false);
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+set_exception_handler([new ErrorHandler(), 'handleException']);
+
+$phpqaPhpunitAutoload = '/tools/.composer/vendor-bin/phpunit/vendor/autoload.php';
+if (file_exists($phpqaPhpunitAutoload)) {
+    require_once $phpqaPhpunitAutoload;
+}
